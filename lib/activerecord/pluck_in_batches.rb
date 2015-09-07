dba4863eedeed956c85139acedafa87f8716cc0d
Bob Lail <bob.lail@cph.org>
2015-09-07 18:25:22 -0500

    [refactor] Extracted activerecord/pluck_in_batches from idioms (4m)

--- lib/activerecord/pluck_in_batches.rb
+++ lib/activerecord/pluck_in_batches.rb
@@ -1,7 +1,27 @@
 require "activerecord/pluck_in_batches/version"
+require "active_record/relation"
 
-module Activerecord
-  module PluckInBatches
-    # Your code goes here...
+module PluckInBatches
+  def pluck_in_batches(*args, &block)
+    options = args.extract_options!
+    in_batches(options) do |batch|
+      batch.pluck(*args).each(&block)
+    end
+  end
+  
+  def in_batches(options = {})
+    options.assert_valid_keys(:batch_size)
+
+    batch_size = options[:batch_size] || 1000
+    batches = (count / batch_size.to_f).ceil
+
+    relation = reorder("#{quoted_table_name}.#{quoted_primary_key} ASC").limit(batch_size)
+    batches.times do |i|
+      yield relation.offset(i * batch_size)
+    end
+
+    nil
   end
 end
+
+ActiveRecord::Relation.send :include, PluckInBatches
