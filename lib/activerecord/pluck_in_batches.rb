f5879dfcc30c737d1be98347882c715baba50708
Bob Lail <bob.lailfamily@gmail.com>
2016-08-15 18:10:04 -0500

    [skip] Changed :batch_size to :of to be compatible with Rails 5 (10m)

--- lib/activerecord/pluck_in_batches.rb
+++ lib/activerecord/pluck_in_batches.rb
@@ -1,18 +1,8 @@
 require "activerecord/pluck_in_batches/version"
 require "active_record/relation"
 
-module PluckInBatches
-  def pluck_in_batches(*args, &block)
-    options = args.extract_options!
-    in_batches(options) do |batch|
-      batch.pluck(*args).each(&block)
-    end
-  end
-
-  def in_batches(options = {})
-    options.assert_valid_keys(:batch_size)
-
-    batch_size = options[:batch_size] || 1000
+module InBatches
+  def in_batches(of: 1000)
     batches = (count / batch_size.to_f).ceil
 
     relation = reorder("#{quoted_table_name}.#{quoted_primary_key} ASC").limit(batch_size)
@@ -27,4 +17,14 @@ module PluckInBatches
   end
 end
 
+module PluckInBatches
+  def pluck_in_batches(*args, &block)
+    options = args.extract_options!
+    in_batches(options) do |batch|
+      batch.pluck(*args).each(&block)
+    end
+  end
+end
+
+ActiveRecord::Relation.send :include, InBatches if Rails.version < "5.0.0"
 ActiveRecord::Relation.send :include, PluckInBatches
